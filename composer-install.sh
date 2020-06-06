579cf84e0e297a009370967d66dd013da87f3f5e
cjreysayago <rey933@gmail.com>
2020-06-06 15:54:47 -0300

    change script

--- composer-install.sh
+++ composer-install.sh
@@ -4,17 +4,17 @@
 echo "Installing composer"
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849>
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
 # Move to /usr/local/bin/ to allow global execution
 echo "Moving to /usr/local/bin (Needs root password)"
-sudo composer.phar /usr/local/bin/composer
+sudo mv ./composer.phar /usr/local/bin/composer
 
 
 # Change OS Path variable in order to make composer binaries available
 echo "Including Composer Binaries in Path"
-#echo "export PATH=$PATH:$HOME/.config/composer/vendor/bin" >> .bashrc
+echo "export PATH=$PATH:$HOME/.config/composer/vendor/bin" >> .bashrc
 
 echo "Done! :)"
